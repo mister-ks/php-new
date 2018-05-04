7ee19a09e0d0652f36d6517a1063eb5bc4df2d4b
Aneks <ks03151103@yahoo.co.jp>
2018-05-04 22:09:19 +0900

    change

--- hello.php
+++ hello.php
@@ -3,26 +3,29 @@
   # 基本のキの字
   echo "hello,world!";
 
-  echo "<br />";
+    echo "<br />";
 
   $my = "hello, my world!";
   print($my);
 
-  echo "<br />";
+    echo "<br />";
 
   $you = "hello, your world!";
   print($you);
+  printf($you);
 
-  echo "<br />";
+    echo "<br />";
 
   echo strlen($my);
 
-  echo "<br />";
+    echo "<br />";
 
   echo md5($my);
 
-  echo "<br />";
+    echo "<br />";
 
   echo levenshtein($my, $you);
+
+
 
 ?>
