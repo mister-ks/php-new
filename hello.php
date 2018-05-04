e2f574270d99602081ab7160e1153ca70e13f26e
Aneks <ks03151103@yahoo.co.jp>
2018-05-04 21:39:53 +0900

    add some lines of code

--- hello.php
+++ hello.php
@@ -3,4 +3,26 @@
   # 基本のキの字
   echo "hello,world!";
 
+  echo "<br />";
+
+  $my = "hello, my world!";
+  print($my);
+
+  echo "<br />";
+
+  $you = "hello, your world!";
+  print($you);
+
+  echo "<br />";
+
+  echo strlen($my);
+
+  echo "<br />";
+
+  echo md5($my);
+
+  echo "<br />";
+
+  echo levenshtein($my, $you);
+
 ?>
